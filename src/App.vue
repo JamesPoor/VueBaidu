433403ef205a97a647639ccb1052f7a1f27a2515
Duo_Bao <502971@qq.com>
2021-12-01 00:09:15 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <Head />
+  <Middle />
+  <Bottom />
 </template>
 
 <script>
 import Head from "./components/Head.vue";
+import Middle from "./components/Middle.vue";
+import Bottom from "./components/Bottom.vue";
 
 export default {
   name: "App",
   components: {
     Head,
+    Middle,
+    Bottom,
   },
 };
 </script>
